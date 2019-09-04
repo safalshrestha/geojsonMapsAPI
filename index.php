fb2699c7aa284946272201e22e7b9d9413c25392
Safal <sth.safal@gmail.com>
2019-09-05 00:01:46 +0100

    Uploading GeoJSON and Changing Color of Icon Added

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<?php session_start(); ?>
+<?php
+if(isset($_FILES['jsonfile'])){
+    $file_name = $_FILES['jsonfile']['name'];
+    $file_tmp =$_FILES['jsonfile']['tmp_name'];
+    $ext = strtolower(end(explode('.',$_FILES['jsonfile']['name'])));
+    if($ext != "geojson" && $ext != "geoJSON"){
+       $error = "Wrong File. Only geojson or geoJSON allowed.";
+    }
+    if($error == null){
+       move_uploaded_file($file_tmp,"geoJson/".$file_name);
+       $filetoload = $file_name;
+       $_SESSION['fileloaded'] = $filetoload;
+       echo "Success";
+    }else{
+       echo "<span style='color: red;'>".$error."</span>";
+    }
+ }
+ if (isset($_SESSION['fileloaded'])){
+   $geojsonfile = $_SESSION['fileloaded'];
+ }
+
+ if (isset($_POST['color'])) {
+   $color = $_POST['color'];
+ }
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -7,37 +33,53 @@
   </head>
   <body>
     <div id="map"></div>
+
+    <div id="options">
+      <form method="POST" action="index.php" enctype="multipart/form-data">
+        Upload GeoJSON file: <input type="file" name="jsonfile" onchange="form.submit()">
+      </form>
+      <form method="POST" action="index.php">
+        Marker Color:
+        <select name="color" onchange="form.submit()">
+          <option value="red" <?php if ($color=="red") echo "selected";?>> Red </option>
+          <option value="blue" <?php if ($color=="blue") echo "selected";?>> Blue </option>
+          <option value="green" <?php if ($color=="green") echo "selected";?>> Green </option>
+        </select>
+      </form>
+    </div>
 <!-- Replace the value of the key parameter with your own API key. -->
 <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAwI2qwx3uN_Q2v941johzEwbaUANHTqO4&callback=initMap">
 </script>
 <script type="text/javascript">
+
   var map;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: {
-      lat: 53.3498,
-      lng: -6.2603
-    }
-  });
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: {
+        lat: 53.3498,
+        lng: -6.2603
+      }
+    });
 
-  // Load GeoJSON.
-  // map.data.loadGeoJson(
-  //    'https://storage.googleapis.com/mapsdevsite/json/google.json');
-  map.data.loadGeoJson('https://data.smartdublin.ie/dataset/58969481-417e-4f5a-b8ea-18b56419d0ed/resource/a38b3d50-96ae-495e-ae69-899d833404cf/download/dccrdpandd.geojson');
+    // Load GeoJSON.
+    // map.data.loadGeoJson(
+    //    'https://storage.googleapis.com/mapsdevsite/json/google.json');
+    //map.data.loadGeoJson('https://data.smartdublin.ie/dataset/58969481-417e-4f5a-b8ea-18b56419d0ed/resource/a38b3d50-96ae-495e-ae69-899d833404cf/download/dccrdpandd.geojson');
+    map.data.loadGeoJson('<?php echo "geoJson/".$geojsonfile; ?>');
+    map.data.setStyle(function(feature) {
+      var color = "<?php echo $color; ?>";
 
-  map.data.setStyle(function(feature) {
-    var color = "green";
+     return {
+     		//icon: 'http://www.myiconfinder.com/uploads/iconsets/256-256-f900504cdc9f243b1c6852985c35a7f7.png',
+        icon: 'icons/'+'<?php echo $color; ?>'+'.png',
+        fillColor: color,
+        strokeWeight: 1
+      }
+    });
 
-   return {
-   		//icon: 'http://www.myiconfinder.com/uploads/iconsets/256-256-f900504cdc9f243b1c6852985c35a7f7.png',
-      fillColor: color,
-      strokeWeight: 1
-    }
-  });
-
-}
+  }
 
 
 </script>
